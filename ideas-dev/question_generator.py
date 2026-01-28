f553f84fc0f83293295720eae80f2672e6ae316e
Sophie <sophie.zihan.perrot@gmail.com>
2026-01-28 23:19:22 +0000

    changed from inline variables to separate csv file

--- ideas-dev/question_generator.py
+++ ideas-dev/question_generator.py
@@ -9,6 +9,8 @@ TODO list
 
 import re
 import random
+import csv
+import os
 
 """
 EXAMPLE QUESTION
@@ -18,32 +20,33 @@ Write your answer in the form y=mx+c, where m and c are integers to be found. [5
 """
 
 example_input = """
-y={a,range:1,10}x^3+{b,range:0,5}x^2+{c,range:0,15}x+{d,range:0,20}
+y={a}x^3+{b}x^2+{c}x+{d}
 Find the equation of the tangent to the curve at the point P({x,range:4,10}, {y}).
 Write your answer in the form y=mx+c, where m and c are integers to be found. [5]
 """
+VARIABLES_FILENAME = os.path.join(os.getcwd(), "variables.csv")
 
-
-def parse_variables(input):
+def parse_variables(var_file):
     variables = {}
-    input_variables = re.findall("{([a-z0-9]+),([^\}]+)}", input)
-    for var_name, rule in input_variables:
-        if var_name not in variables.keys():
-            variables[var_name] = rule
-    return variables
+    with open(var_file) as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            variables[row["name"]] = row["rule"]
+        return variables
 
 def parse_rule(rule):
     rule_type, rule_params = re.split(":", rule)
     if rule_type == "range":
-        start, end = re.split(",", rule_params)
+        start, end = re.split("-", rule_params)
         return random.randint(int(start), int(end))
 
-def generate_new_question(question_template, variables):
+def generate_new_question(question_template, var_file):
+    variables = parse_variables(var_file)
     for variable, rule in variables.items():
-        pattern = "{" + variable + "," + rule + "}"
+        pattern = "{" + variable + "}"
         new_val = str(parse_rule(rule))
         question_template = re.sub(pattern, new_val, question_template) 
     return question_template
 
-variables = parse_variables(example_input)
-print(generate_new_question(example_input, variables))
+
+print(generate_new_question(example_input, VARIABLES_FILENAME))
